d91a2405195875fafcdf13cbac3238c0369cbdb7
rigolao <mrigolao@gmail.com>
2023-09-23 14:42:51 -0300

    Nova tela de login

--- lib/home/home_content.dart
+++ lib/home/home_content.dart
@@ -31,24 +31,7 @@ class _HomeContentState extends State<HomeContent>{
         title: const Text('Natação Unaerp', style: TextStyle(color: Colors.white)),
       ),
       body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.sports),
-            label: 'Meus Treinos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'Historico',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-          ),
-        ],
-      )
+      bottomNavigationBar: BottomTreinador(_currentIndex, _onTabTapped),
     );
   }
 }
@@ -79,3 +62,97 @@ class Page3 extends StatelessWidget {
     );
   }
 }
+
+class BottomAtleta extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTabTapped;
+
+  const BottomAtleta(this.currentIndex, this.onTabTapped, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTabTapped,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sports),
+          label: 'Meus Treinos',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.history),
+          label: 'Historico',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Perfil',
+        ),
+      ],
+    );
+  }
+}
+
+class BottomTreinador extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTabTapped;
+
+  const BottomTreinador(this.currentIndex, this.onTabTapped, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTabTapped,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sports),
+          label: 'Atletas',
+        ),
+        BottomNavigationBarItem(
+          icon: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              child: Icon(Icons.add_circle_outline, size: 40),
+            ),
+          ),
+          label: "",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Perfil',
+        ),
+      ],
+    );
+  }
+}
+
+class BottomAdmin extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTabTapped;
+
+  const BottomAdmin(this.currentIndex, this.onTabTapped, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTabTapped,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sports),
+          label: 'Meus Treinos',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.history),
+          label: 'Historico',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Perfil',
+        ),
+      ],
+    );
+  }
+}
+
+
--- lib/login/login_content.dart
+++ lib/login/login_content.dart
@@ -1,5 +1,8 @@
 import 'package:desafio_6_etapa/home/home_content.dart';
+import 'package:desafio_6_etapa/login/teste.dart';
 import 'package:flutter/material.dart';
+
+import '../theme/theme.g.dart';
 
 class LoginContent extends StatefulWidget {
   const LoginContent({Key? key}) : super(key: key);
@@ -39,76 +42,110 @@ class _LoginContentState extends State<LoginContent> {
   @override
   Widget build(BuildContext context) {
 
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text('Natação Unaerp', style: TextStyle(color: Colors.white)),
-      ),
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  width: 250,
-                  height: 250,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/logo.png'),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'E-mail',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Coloque seu email';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _senhaController,
-                  obscureText: !_mostrarSenha,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _mostrarSenha
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                      ),
-                      onPressed: _alterarVisibilidadeSenha,
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Coloque sua senha';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                FilledButton(
-                  onPressed: _logar,
-                  child: const Text('Login'),
-                ),
-              ],
+    return Material(
+      child: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/login_background.png'),
+                fit: BoxFit.fill,
+              ),
             ),
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Container(
+                      padding:
+                      const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 4,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 64),
+                          Text('Identificação',
+                              style: TextStyle(fontSize: 24, color: lightColorScheme.primary, fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 48),
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'E-mail',
+                              suffixIcon: Icon(Icons.person)
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Coloque seu email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _senhaController,
+                            obscureText: !_mostrarSenha,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              labelText: 'Senha',
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _mostrarSenha
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                ),
+                                onPressed: _alterarVisibilidadeSenha,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Coloque sua senha';
+                              }
+                              return null;
+                            },
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                                onPressed: () => {},
+                                child: const Text('ESQUECI MINHA SENHA')),
+                          ),
+                          const SizedBox(height: 32),
+                          FilledButton(onPressed: () => {}, child: const Text('Entrar')),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/logo1.png'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
