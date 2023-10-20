b4b548edbdc3a8f30d13ea78744c8e39def7fa97
rigolao <mrigolao@gmail.com>
2023-10-20 20:27:30 -0300

    Fix: Arrumando apresentação.

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,3 +1,4 @@
+import 'package:desafio_6_etapa/entity/tipo_usuario.dart';
 import 'package:desafio_6_etapa/finalizar_cadastro/finalizar_cadastro_content.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class AppController extends ChangeNotifier {
 
   void isUsuarioCompleted(BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (_state.usuario == null) {
+      if (_state.usuario?.tipoUsuario == TipoUsuario.ATLETA) {
         Navigator.push(context, MaterialPageRoute(builder: (context) {
           return const FinalizarCadastroContent();
         }));
