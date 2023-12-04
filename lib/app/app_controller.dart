4d1f98da078ef66a8711f0ec138424444c3a8cc2
jomoliveira <jomoliveira@unaerp.br>
2023-12-04 18:37:03 -0300

    Feat: Cadastro completo do atleta e autenticando se é a primeira vez logado do usuário

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:desafio_6_etapa/entity/tipo_usuario.dart';
 import 'package:desafio_6_etapa/entity/treino.dart';
 import 'package:desafio_6_etapa/finalizar_cadastro/finalizar_cadastro_content.dart';
@@ -22,13 +23,45 @@ class AppController extends ChangeNotifier {
   }
 
   void isUsuarioCompleted(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
       if (_state.usuario?.tipoUsuario == TipoUsuario.ATLETA) {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return const FinalizarCadastroContent();
-        }));
+        bool primeiroAcesso = await verificarPrimeiroAcesso(_state.usuario?.id);
+
+        if (primeiroAcesso) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const FinalizarCadastroContent();
+          }));
+        }
       }
       notifyListeners();
     });
   }
+
+  Future<bool> verificarPrimeiroAcesso(String? uuid) async {
+    CollectionReference usuarios = FirebaseFirestore.instance.collection('usuarios');
+
+    try {
+      QuerySnapshot querySnapshot = await usuarios.where('uuid', isEqualTo: uuid).get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        bool primeiroAcesso = querySnapshot.docs.first['primeiroAcesso'];
+
+        return primeiroAcesso;
+      } else {
+        return true;
+      }
+    } catch (e) {
+      print('Erro ao realizar a consulta: $e');
+      return true;
+    }
+  }
+
+
+  void main() async {
+    // Uso da função
+    String uuid = 'seu_uuid_aqui';
+    bool primeiroAcesso = await verificarPrimeiroAcesso(uuid);
+    print('O usuário realizou o primeiro acesso? $primeiroAcesso');
+  }
+
 }
--- lib/finalizar_cadastro/finalizar_cadastro_controller.dart
+++ lib/finalizar_cadastro/finalizar_cadastro_controller.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:desafio_6_etapa/app/app_controller.dart';
 import 'package:desafio_6_etapa/entity/atleta.dart';
 import 'package:desafio_6_etapa/entity/tipo_usuario.dart';
 import 'package:desafio_6_etapa/finalizar_cadastro/pages/dados_basicos.dart';
@@ -5,6 +7,7 @@ import 'package:desafio_6_etapa/finalizar_cadastro/pages/endereco_content.dart';
 import 'package:desafio_6_etapa/finalizar_cadastro/pages/imagens_content.dart';
 import 'package:desafio_6_etapa/finalizar_cadastro/pages/outros_content.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'finalizar_cadastro_state.dart';
 
@@ -22,7 +25,6 @@ class FinalizarCadastroController extends ChangeNotifier {
       id: '0',
       nome: '',
       email: '',
-      senha: '',
       sexo:  '',
       telefones: [],
       tipoUsuario: TipoUsuario.ATLETA,
@@ -47,6 +49,56 @@ class FinalizarCadastroController extends ChangeNotifier {
     ),
   );
 
+  Future<String?> criarAtleta(BuildContext context, Map<String, dynamic> atletaDados) async {
+    final AppController appController = Provider.of<AppController>(context, listen: false);
+
+   final usuario = appController.state.usuario;
+
+   final usuarios = FirebaseFirestore.instance.collection('usuarios');
+
+   await usuarios.doc(usuario!.id).set(atletaDados, SetOptions(merge: true));
+
+   return usuario.id;
+
+
+  }
+
+  Atleta criarAtleta1 (BuildContext context){
+    final AppController appController = Provider.of<AppController>(context, listen: false);
+
+    final usuario = appController.state.usuario;
+
+    debugPrint(usuario!.nome);
+
+    return Atleta(
+      id: usuario!.id,
+      nome: usuario.nome,
+      email: usuario.email,
+      tipoUsuario: usuario.tipoUsuario,
+      dataNascimento: DateTime.now(),
+      naturalidade: state.naturalidadeController.text,
+      nacionalidade: state.nacionalidadeController.text,
+      rg: state.rgController.text,
+      cpf:state.cpfController.text,
+      sexo: '',
+      endereco: state.enderecoController.text,
+      bairro: state.bairroController.text,
+      cep: state.cepController.text,
+      cidade: state.cidadeController.text,
+      estado: state.estadoController.text,
+      nomeMae: state.nomeMaeController.text,
+      nomePai: state.nomePaiController.text,
+      clubeOrigem: state.clubeOrigemController.text,
+      empresaTrabalha: state.empresaTrabalhaController.text,
+      cvm: state.cvmController.text,
+      alg: state.alergiaController.text,
+      est: state.estadoController.text,
+      pvr: state.pvrController.text,
+      telefones: [],
+    );
+
+  }
+
   void changePage(int index) {
     _state.currentIndex = index;
     notifyListeners();
@@ -68,6 +120,14 @@ class FinalizarCadastroController extends ChangeNotifier {
 
   void finalizarCadastro(BuildContext context) {
     if (_state.currentIndex == _state.pages.length - 1) {
+
+      Atleta atleta = criarAtleta1(context);
+
+      debugPrint('Nome da mae: ${atleta.nomeMae}');
+      debugPrint(atleta.nomePai);
+      debugPrint(atleta.nome);
+      criarAtleta(context, atleta.toJson());
+
       showDialog(context: context, builder: (BuildContext context) {
         return AlertDialog(
           title: const Text('Cadastro finalizado'),
@@ -75,7 +135,6 @@ class FinalizarCadastroController extends ChangeNotifier {
           actions: [
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop();
                 Navigator.of(context).pop();
               },
               child: const Text('OK'),
